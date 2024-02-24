d90ffb7bad99745bedee9e050e039e2281c2a3ed
TomsLiderUZ <lidirgame@gmail.com>
2024-02-24 20:39:13 +0500

    save-1

--- Contents/Home.js
+++ Contents/Home.js
@@ -7,12 +7,12 @@ let setContentHomePage = `
               </div>
             </div>
             <div class="contentCaruselPageItemsBg">
-              <div class="contentCaruselPage_1 contentCaruselPageOwn contentCaruselPage"></div>
-              <div class="contentCaruselPage_2 contentCaruselPageNotOwn contentCaruselPage"></div>
-              <div class="contentCaruselPage_3 contentCaruselPageNotOwn contentCaruselPage"></div>
-              <div class="contentCaruselPage_4 contentCaruselPageNotOwn contentCaruselPage"></div>
-              <div class="contentCaruselPage_5 contentCaruselPageNotOwn contentCaruselPage"></div>
-              <div class="contentCaruselPage_6 contentCaruselPageNotOwn contentCaruselPage"></div>
+              <div class="contentCaruselPage_1 contentCaruselPageOwn contentCaruselPage">1</div>
+              <div class="contentCaruselPage_2 contentCaruselPageNotOwn contentCaruselPage">2</div>
+              <div class="contentCaruselPage_3 contentCaruselPageNotOwn contentCaruselPage">3</div>
+              <div class="contentCaruselPage_4 contentCaruselPageNotOwn contentCaruselPage">4</div>
+              <div class="contentCaruselPage_5 contentCaruselPageNotOwn contentCaruselPage">5</div>
+              <div class="contentCaruselPage_6 contentCaruselPageNotOwn contentCaruselPage">6</div>
             </div>
             <div class="contentCaruselPageNextBtnBg contentCaruselPageBtnBg">
               <div class="contentCaruselPageNextBtn">
@@ -22,7 +22,7 @@ let setContentHomePage = `
         </div>
 
 `
-document.querySelector(".conentsBg").innerHTML = setContentHomePage
+// document.querySelector(".conentsBg").innerHTML = setContentHomePage
 
 
 
@@ -30,7 +30,27 @@ let contentCaruselPageNext = 1;
 let contentCaruselPageClicked = true
 setInterval(() => {
     contentCaruselPageNext += 1
-}, 5000);
+}, 10000);
+
+document.querySelector(".contentCaruselPageNumber_1").addEventListener("click", ()=>{
+    contentCaruselPageNext = 1;
+});
+document.querySelector(".contentCaruselPageNumber_2").addEventListener("click", ()=>{
+    contentCaruselPageNext = 2;
+});
+document.querySelector(".contentCaruselPageNumber_3").addEventListener("click", ()=>{
+    contentCaruselPageNext = 3;
+});
+document.querySelector(".contentCaruselPageNumber_4").addEventListener("click", ()=>{
+    contentCaruselPageNext = 4;
+});
+document.querySelector(".contentCaruselPageNumber_5").addEventListener("click", ()=>{
+    contentCaruselPageNext = 5;
+});
+document.querySelector(".contentCaruselPageNumber_6").addEventListener("click", ()=>{
+    contentCaruselPageNext = 6;
+});
+
 document.querySelector(".contentCaruselPageNextBtnBg").addEventListener("click", ()=>{
     if (contentCaruselPageClicked === true) {
         contentCaruselPageClicked = false
@@ -68,6 +88,13 @@ setInterval(() => {
             document.querySelector(".contentCaruselPage_5").classList.remove("contentCaruselPageOwn")
             document.querySelector(".contentCaruselPage_6").classList.add("contentCaruselPageNotOwn")
             document.querySelector(".contentCaruselPage_6").classList.remove("contentCaruselPageOwn")
+
+            document.querySelector(".contentCaruselPageNumber_1").classList.add("contentCaruselPageNumberOwn")
+            document.querySelector(".contentCaruselPageNumber_2").classList.remove("contentCaruselPageNumberOwn")
+            document.querySelector(".contentCaruselPageNumber_3").classList.remove("contentCaruselPageNumberOwn")
+            document.querySelector(".contentCaruselPageNumber_4").classList.remove("contentCaruselPageNumberOwn")
+            document.querySelector(".contentCaruselPageNumber_5").classList.remove("contentCaruselPageNumberOwn")
+            document.querySelector(".contentCaruselPageNumber_6").classList.remove("contentCaruselPageNumberOwn")
             break;
         case 2:
         document.querySelector(".contentCaruselPage_2").classList.add("contentCaruselPageOwn")
@@ -83,6 +110,13 @@ setInterval(() => {
         document.querySelector(".contentCaruselPage_5").classList.remove("contentCaruselPageOwn")
         document.querySelector(".contentCaruselPage_6").classList.add("contentCaruselPageNotOwn")
         document.querySelector(".contentCaruselPage_6").classList.remove("contentCaruselPageOwn")
+
+        document.querySelector(".contentCaruselPageNumber_1").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_2").classList.add("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_3").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_4").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_5").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_6").classList.remove("contentCaruselPageNumberOwn")
             break;
         case 3:
         document.querySelector(".contentCaruselPage_3").classList.add("contentCaruselPageOwn")
@@ -98,6 +132,13 @@ setInterval(() => {
         document.querySelector(".contentCaruselPage_5").classList.remove("contentCaruselPageOwn")
         document.querySelector(".contentCaruselPage_6").classList.add("contentCaruselPageNotOwn")
         document.querySelector(".contentCaruselPage_6").classList.remove("contentCaruselPageOwn")
+
+        document.querySelector(".contentCaruselPageNumber_1").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_2").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_3").classList.add("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_4").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_5").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_6").classList.remove("contentCaruselPageNumberOwn")
             break;
         case 4:
         document.querySelector(".contentCaruselPage_4").classList.add("contentCaruselPageOwn")
@@ -113,6 +154,13 @@ setInterval(() => {
         document.querySelector(".contentCaruselPage_5").classList.remove("contentCaruselPageOwn")
         document.querySelector(".contentCaruselPage_6").classList.add("contentCaruselPageNotOwn")
         document.querySelector(".contentCaruselPage_6").classList.remove("contentCaruselPageOwn")
+
+        document.querySelector(".contentCaruselPageNumber_1").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_2").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_3").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_4").classList.add("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_5").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_6").classList.remove("contentCaruselPageNumberOwn")
             break;
         case 5:
         document.querySelector(".contentCaruselPage_5").classList.add("contentCaruselPageOwn")
@@ -128,6 +176,13 @@ setInterval(() => {
         document.querySelector(".contentCaruselPage_2").classList.remove("contentCaruselPageOwn")
         document.querySelector(".contentCaruselPage_6").classList.add("contentCaruselPageNotOwn")
         document.querySelector(".contentCaruselPage_6").classList.remove("contentCaruselPageOwn")
+
+        document.querySelector(".contentCaruselPageNumber_1").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_2").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_3").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_4").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_5").classList.add("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_6").classList.remove("contentCaruselPageNumberOwn")
             break;
         case 6:
         document.querySelector(".contentCaruselPage_6").classList.add("contentCaruselPageOwn")
@@ -143,6 +198,13 @@ setInterval(() => {
         document.querySelector(".contentCaruselPage_5").classList.remove("contentCaruselPageOwn")
         document.querySelector(".contentCaruselPage_2").classList.add("contentCaruselPageNotOwn")
         document.querySelector(".contentCaruselPage_2").classList.remove("contentCaruselPageOwn")
+
+        document.querySelector(".contentCaruselPageNumber_1").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_2").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_3").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_4").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_5").classList.remove("contentCaruselPageNumberOwn")
+        document.querySelector(".contentCaruselPageNumber_6").classList.add("contentCaruselPageNumberOwn")
             break;
         case 7:
             contentCaruselPageNext = 1;
